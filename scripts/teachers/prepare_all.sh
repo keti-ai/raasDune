95f006f6d5d420ad4dd7766879c97a2ad1ef6917
syh4661 <syh20131292@gmail.com>
2025-07-17 16:07:13 +0200

    edit for NLE side syh laptop

--- scripts/teachers/prepare_all.sh
+++ scripts/teachers/prepare_all.sh
@@ -7,6 +7,6 @@ MODELS_ROOT_DIR=${1}
 
 umask 000
 
-bash dinov2.sh ${MODELS_ROOT_DIR}
+#bash dinov2.sh ${MODELS_ROOT_DIR}
 bash mast3r.sh ${MODELS_ROOT_DIR} # gcc-toolset-9 needed
 bash multihmr.sh ${MODELS_ROOT_DIR}
--- teachers/config.py
+++ teachers/config.py
@@ -5,7 +5,7 @@ from .vit_master import mast3r
 TEACHER_CFG = {
     "dino2reg_vitlarge_14": {
         "loader": dinov2_vitlarge,
-        "ckpt_path": "/path/to/dinov2reg/vitlarge14/checkpoint.pth",
+        "ckpt_path": "/home/keti/checkpoints/dinov2reg/vitlarge14/checkpoint.pth",
         "ckpt_key": "",
         "num_features": 1024,
         "image_size": 518,
@@ -16,9 +16,9 @@ TEACHER_CFG = {
     },
     "mast3r_vitlarge_16": {
         "loader": mast3r,
-        "ckpt_path": "/path/to/mast3r/vitlarge16/checkpoint.pth",
+        "ckpt_path": "/home/keti/checkpoints/mast3r/vitlarge16/checkpoint.pth",
         "ckpt_key": None,
-        "code_dir": "/path/to/mast3r/code",  # Path to the MAST3R code, downloaded by scripts/teachers/mast3r.sh
+        "code_dir": "/home/keti/checkpoints/mast3r/code",  # Path to the MAST3R code, downloaded by scripts/teachers/mast3r.sh
         "num_features": 1024,
         "image_size": 512,  # shortest side
         "patch_size": 16,
@@ -26,7 +26,7 @@ TEACHER_CFG = {
     },
     "multihmr_vitlarge_14_672": {
         "loader": dinov2_vitlarge,
-        "ckpt_path": "/path/to/multihmr/vitlarge14_672/checkpoint.pth",
+        "ckpt_path": "/home/keti/checkpoints/vitlarge14_672/checkpoint.pth",
         "ckpt_key": "model_state_dict",
         "num_features": 1024,
         "image_size": 518,  # model has 37 ** 2 + 1 positional embeddings, but fine-tuned at resolution 672
